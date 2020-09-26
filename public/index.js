bb2c262aa24535698c45a4e5f9805be293ff9b5a
Saumya <saumya.singh203@gmail.com>
2020-09-26 19:11:18 +0530

    new interface

---
+++ public/index.js
@@ -0,0 +1,21 @@
+
+
+
+$('.count').counterUp({
+  		delay:10,
+  		time:300
+   	})
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -18,7 +18,7 @@ const Countries = ({ handleCountryChange }) => {
   }, []);
 
   return (
-    <FormControl className="btn btn-primary">
+    <FormControl className={styles.dropDown}>
       <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
         <option value="">Global</option>
         {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
